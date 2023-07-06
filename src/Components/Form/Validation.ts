1ad2c0a718d5a402fd15fedc2f5de596b15d7b32
IggyMori <115964314+IggyMori@users.noreply.github.com>
2023-07-06 15:44:30 +0500

    validation fix

--- src/Components/Form/Validation.ts
+++ src/Components/Form/Validation.ts
@@ -3,30 +3,6 @@ error: boolean,
  message: string
 }
 
-const setValidationValue = (
-    isValid: boolean,
-    name: string,
-    message: string,
-    setValidation : (arg: any) => void,
-    validation: any,
-) => {
-    isValid ?
-        setValidation({
-            ...validation,
-            [name]: {
-                error: false,
-                message: message,
-            },
-        })
-        :
-        setValidation({
-            ...validation,
-            [name]: {
-                error: true,
-                message: message,
-            },
-        });
-}
 export const validationHandler = (
         name: string,
         value: string,
@@ -45,58 +21,64 @@ export const validationHandler = (
         switch (name){
             case "email":
                 value.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)
-                    ? setValidationValue(
-                        true,
-                        name,
-                        "",
-                        validation,
-                        setValidation
-                    )
-                    : setValidationValue(
-                        false,
-                        name,
-                        "Неверный формат Email!",
-                        validation,
-                        setValidation
-                    );
+                    ?
+                    setValidation({
+                        ...validation,
+                        [name]: {
+                            error: false,
+                            message: '',
+                        },
+                    })
+                    :
+                    setValidation({
+                        ...validation,
+                        [name]: {
+                            error: true,
+                            message: "Неверный формат Email!",
+                        },
+                    })
+
                 break;
 
             case "password":
                 value.length >= 6
                 ?
-                    setValidationValue(
-                        true,
-                        name,
-                        "",
-                        validation,
-                        setValidation
-                    )
-                    : setValidationValue(
-                        false,
-                        name,
-                        "Должно быть не менее 6 символов",
-                        validation,
-                        setValidation
-                    );
+                    setValidation({
+                        ...validation,
+                        [name]: {
+                            error: false,
+                            message: '',
+                        },
+                    })
+                    :
+                    setValidation({
+                        ...validation,
+                        [name]: {
+                            error: true,
+                            message: "Должно быть не менее 6 символов",
+                        },
+                    })
 
                 break;
             case "phone":
                 value.match(/^998\d{9}$/)
                     ?
-                    setValidationValue(
-                        true,
-                        name,
-                        "",
-                        validation,
-                        setValidation
-                    )
-                    : setValidationValue(
-                        false,
-                        name,
-                        "Неверный формат номера",
-                        validation,
-                        setValidation
-                    );
+
+                    setValidation({
+                        ...validation,
+                        [name]: {
+                            error: false,
+                            message: '',
+                        },
+                    })
+                    :
+                    setValidation({
+                        ...validation,
+                        [name]: {
+                            error: true,
+                            message: "Неверный формат номера",
+                        },
+                    })
                 break;
 
         }
